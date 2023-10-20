373b1a885ec5253179936ac7a731ef6bba48760a
Elham Barezi <57970163+ebarezi@users.noreply.github.com>
2023-10-19 22:10:08 -0400

    first

---
+++ HTML Editor - Web Content Composer_files/productData_womens.js
@@ -0,0 +1,103 @@
+// put featured shoe second in the order so it appears first after the intro screen
+
+cold2019 = [
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/ridge_mini.png",
+            name: "Ridge Mini",
+            price: "$160",
+            url: "https://www.ugg.com/women-cold-weather-boots/ridge-mini/1103840.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack.png",
+            name: "Adirondack III",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/adirondack-iii-boot/1095141.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack_fluff.png",
+            name: "Adirondack III Fluff",
+            price: "$250",
+            url: "https://www.ugg.com/women-boots/adirondack-boot-iii-fluff/1103849.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/tahoe.png",
+            name: "Tahoe",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/tahoe/1103848.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/montara.png",
+            name: "Montara",
+            price: "$225",
+            url: "https://www.ugg.com/women-cold-weather-boots/montara/1106770.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        }
+        
+    ],
+
+rain2019 = [
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack_fluff.png",
+            name: "Adirondack III Fluff",
+            price: "$250",
+            url: "https://www.ugg.com/women-boots/adirondack-boot-iii-fluff/1103849.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/tahoe.png",
+            name: "Tahoe",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/tahoe/1103848.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/montara.png",
+            name: "Montara",
+            price: "$225",
+            url: "https://www.ugg.com/women-cold-weather-boots/montara/1106770.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/ridge_mini.png",
+            name: "Ridge Mini",
+            price: "$160",
+            url: "https://www.ugg.com/women-cold-weather-boots/ridge-mini/1103840.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack.png",
+            name: "Adirondack III",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/adirondack-iii-boot/1095141.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        }
+    ],
+
+snow2019 = [
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/montara.png",
+            name: "Montara",
+            price: "$225",
+            url: "https://www.ugg.com/women-cold-weather-boots/montara/1106770.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/ridge_mini.png",
+            name: "Ridge Mini",
+            price: "$160",
+            url: "https://www.ugg.com/women-cold-weather-boots/ridge-mini/1103840.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack.png",
+            name: "Adirondack III",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/adirondack-iii-boot/1095141.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/adirondack_fluff.png",
+            name: "Adirondack III Fluff",
+            price: "$250",
+            url: "https://www.ugg.com/women-boots/adirondack-boot-iii-fluff/1103849.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        },
+        {
+            src: "https://s0.2mdn.net/creatives/assets/3491627/tahoe.png",
+            name: "Tahoe",
+            price: "$250",
+            url: "https://www.ugg.com/women-cold-weather-boots/tahoe/1103848.html?utm_source=googledv&utm_medium=adn_dis&utm_campaign=womensweather"
+        }
+        
+        
+    ]
